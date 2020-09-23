b0be89cfd2efdc00f53e34825ff17c1d102d95b7
placid-tier-4550 <james.ardery@gmail.com>
2020-09-23 23:38:28 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -6,8 +6,8 @@ row_index = 0
 while row_index < src.count do
   element_index = 0
   while element_index < src[row_index].count do
-     if element_index % 2 == 0
-     p row_index[element_index]
+     if src[row_index][element_index][0] % 2 == 0
+     p src[row_index][element_index]
      end
    element_index += 1
   end
